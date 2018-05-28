a3418e22ab42dfba77513e8bf84f42702ccdd8b0
Isaac Massot <isaac.mg@coditramuntana.com>
2018-05-28 10:33:35 +0200

    change to decidim form helper

--- decidim-core/app/helpers/decidim/application_helper.rb
+++ decidim-core/app/helpers/decidim/application_helper.rb
@@ -32,9 +32,5 @@ module Decidim
 
       presenter
     end
-
-    def foundation_datepicker_locale_tag
-      javascript_include_tag "datepicker-locales/foundation-datepicker.#{I18n.locale}.js" if I18n.locale != :en
-    end
   end
 end
