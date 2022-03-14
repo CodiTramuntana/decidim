608930bc5e968d617ea752c01c8a4c225839137b
Laura Jaime <laura.jv@coditramuntana.com>
2022-03-14 13:22:32 +0100

    System: check automate steps in organization

--- decidim-system/app/commands/decidim/system/register_organization.rb
+++ decidim-system/app/commands/decidim/system/register_organization.rb
@@ -54,6 +54,7 @@ module Decidim
           available_authorizations: form.clean_available_authorizations,
           users_registration_mode: form.users_registration_mode,
           force_users_to_authenticate_before_access_organization: form.force_users_to_authenticate_before_access_organization,
+          automate_steps_in_processes: form.automate_steps_in_processes,
           badges_enabled: true,
           user_groups_enabled: true,
           default_locale: form.default_locale,
