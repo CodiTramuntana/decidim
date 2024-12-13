a105bab8380e196ef7ccb3603e4f43aa3a24cd8a
Laura Jaime <laura.jv@coditramuntana.com>
2024-12-13 10:51:23 +0100

    Fix active link templates in admin

--- decidim-admin/config/routes.rb
+++ decidim-admin/config/routes.rb
@@ -18,7 +18,6 @@ Decidim::Admin::Engine.routes.draw do
     Decidim.participatory_space_manifests.each do |manifest|
       mount manifest.context(:admin).engine, at: "/", as: "decidim_admin_#{manifest.name}"
     end
-    mount Decidim::Templates::AdminEngine, at: "/", as: "decidim_admin_templates"
 
     resources :static_pages do
       put :update_content_blocks, on: :member
