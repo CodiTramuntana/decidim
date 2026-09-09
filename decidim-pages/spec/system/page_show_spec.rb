ad4d28dfe24331bd11b12fefefd7863a8482ee6e
Ruben <ruben.bg@coditramuntana.com>
2026-09-09 09:20:06 +0200

    Add Bullet safelists

--- decidim-pages/spec/system/page_show_spec.rb
+++ decidim-pages/spec/system/page_show_spec.rb
@@ -93,6 +93,13 @@ describe "Show a page" do
       let!(:photo) { create(:attachment, :with_image, attached_to: page_component) }
 
       before do
+        Bullet.add_safelist type: :unused_eager_loading, class_name: "Decidim::Attachment", association: :file_attachment
+        Bullet.add_safelist type: :unused_eager_loading, class_name: "ActiveStorage::Attachment", association: :blob
+        Bullet.add_safelist type: :unused_eager_loading, class_name: "ActiveStorage::Attachment", association: :record
+        Bullet.add_safelist type: :unused_eager_loading, class_name: "ActiveStorage::Blob", association: :variant_records
+        Bullet.add_safelist type: :unused_eager_loading, class_name: "ActiveStorage::Blob", association: :preview_image_attachment
+        Bullet.add_safelist type: :unused_eager_loading, class_name: "Decidim::Attachment", association: :attachment_collection
+
         visit_component
       end
 
